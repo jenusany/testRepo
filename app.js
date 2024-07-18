b6631790019b7cc4c5f27bad384e1d67f41c6e5f
jenusany <jenusany@outlook.com>
2024-07-18 11:59:09 -0400

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.getElementById("test").addEventListener("click", ()=> sendData("asd"))
 
 
 async function sendData(data) {
-    const response = await fetch('http://127.0.0.1:5000/process', {
+    const response = await fetch('http://127.0.0.1:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
