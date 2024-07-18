d411e981dc8b94276905c0b3ea055fa9b464aaf3
jenusany <jenusany@outlook.com>
2024-07-18 11:41:47 -0400

    flask work

--- app.js
+++ app.js
@@ -1,73 +1,29 @@
-document.getElementById('loginButton').onclick = function() {
-    const appId = '803246971960239';
-    const redirectUri = 'https://jenusany.github.io/testRepo/';
-    const authUrl = `https://www.facebook.com/v11.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=instagram_basic,pages_show_list,business_management,pages_read_engagement`;
-  
-    window.location.href = authUrl;
-  };
-  const urlParams = new URLSearchParams(window.location.search);
-const code = urlParams.get('code');
+document.getElementById("test").addEventListener("click", ()=> sendData("asd"))
 
-if (code) {
-// Exchange the code for an access token
-fetch(`https://graph.facebook.com/v11.0/oauth/access_token?client_id=803246971960239&redirect_uri=https://jenusany.github.io/testRepo/&client_secret=c19298b4ce75926bf2dc0177b77e5912&code=${code}`)
-  .then(response => response.json())
-  .then(data => {
-    const accessToken = data["access_token"];
 
-    fetch(`https://graph.facebook.com/v20.0/me/businesses?access_token=${accessToken}`)
-        .then(response => response.json())
-        .then(data => {
-            let businesses = data["data"]
-            const businessesHTML = document.getElementById('businesses')
-            for(let i = 0; i < businesses.length; i++){
-              const portfolio = document.createElement("button");
-              portfolio.innerText = businesses[i]["name"]
-              businessesHTML.appendChild(portfolio)
-              portfolio.addEventListener("click", () => {
-                console.log(businesses[i]["name"])
-                ownedPages(businesses[i]["id"], accessToken)
-              })
-            }
-        })
-  })
-  .catch(error => console.error('Error:', error));
+async function sendData(data) {
+    const response = await fetch('http://127.0.0.1:5000/process', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    const result = await response.json();
+    return result;
 }
 
-function ownedPages(pageid, accessToken){
-  fetch(`https://graph.facebook.com/v14.0/${pageid}/owned_pages?access_token=${accessToken}`)
-    .then(response => response.json())
-    .then(data => {
-      const accounts = data["data"]
-      console.log(accounts)
-      const accountsHTML = document.getElementById('accounts')
-      for(let i = 0; i < accounts.length; i++){
-        const indAcc = document.createElement("button");
-        indAcc.innerText = accounts[i]["name"]
-        accountsHTML.appendChild(indAcc)
-        indAcc.addEventListener("click", () => {
-          instaAccount(accounts[i]["id"], accessToken)
-        })
-      }
+// Example usage
+const data = { key: 'value' };
+sendData(data)
+    .then(result => {
+        console.log('Processed data:', result);
     })
-}
-
-function instaAccount(pageid, accessToken){
-  fetch(`https://graph.facebook.com/v20.0/${pageid}?fields=instagram_business_account&access_token=${accessToken}`)
-    .then(response => response.json())
-    .then(data => {
-      const id = data["instagram_business_account"]["id"]
-      fetch(`https://graph.facebook.com/v20.0/${id}/media?access_token=${accessToken}`)
-      .then(response => response.json())
-      .then(data => {
-        const posts = data["data"]
-        for(let i = 0; i < posts.length; i++){
-          fetch(`https://graph.facebook.com/v20.0/${posts[i]}/media?fields=media_url&access_token=${accessToken}`)
-          .then(response => console.log(response))
-        }
-
-
-      })
-
-    })
-}
+    .catch(error => {
+        console.error('Error:', error);
+    });
---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/process', methods=['POST'])
+def process():
+    data = request.get_json()
+    # Perform your Python operations on the data here
+    processed_data = data_processing_function(data)
+    return jsonify(processed_data)
+
+def data_processing_function(data):
+    # Replace this with your actual data processing logic
+    processed_data = {"result": "processed", "original": data}
+    return processed_data
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
