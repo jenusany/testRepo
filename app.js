e794926972557197f8f7c0cf3bc7e9bf1008361a
jenusany <jenusany@outlook.com>
2024-07-18 12:21:04 -0400

    no proces

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.getElementById("test").addEventListener("click", ()=> {
 
 
 async function sendData(data) {
-    const response = await fetch('http://127.0.0.1:5000/process', {
+    const response = await fetch('http://127.0.0.1:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
